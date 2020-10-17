550757531ce836b68f40703a1bb2acd65f8d6524
Janet Lee <jlee25@gmail.com>
2020-10-16 23:38:26 -0400

    added color pencil feature

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function createGrid(numOfRows) {
   let squares = document.querySelector(".squares");
 
   // reset grid
-  let cells = document.querySelectorAll(".cell");
+  var cells = document.querySelectorAll(".cell");
   cells.forEach((cell) => squares.removeChild(cell));
 
   squares.style.gridTemplateColumns = `repeat(${numOfRows}, 1fr)`;
@@ -21,19 +21,38 @@ function createGrid(numOfRows) {
   for (let i = 1; i <= numOfCells; i++) {
     let cell = document.createElement("div");
     cell.className = "cell";
+    cell.style.opacity = 0.1;
     squares.appendChild(cell);
   }
 
-  colorCells();
+  darkenCells("blue");
 }
 
-function colorCells() {
+function colorCells(cellColor) {
   let cells = document.querySelectorAll(".cell");
   cells.forEach((cell) =>
     cell.addEventListener("mouseenter", () => {
-      cell.style.backgroundColor = "purple";
+      cell.style.setProperty("--cell-background-color", `${cellColor}`);
+    })
+  );
+}
+
+function darkenCells(cellColor) {
+  var cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) =>
+    cell.addEventListener("mouseenter", () => {
+      //   cell.style.setProperty("--cell-background-color", `${cellColor}`);
+      //   cell.style.setProperty("--cell-opacity", "0.1");
+      cell.style.backgroundColor = `${cellColor}`;
+      let op = parseFloat(cell.style.opacity);
+      if (op < 1) {
+        cell.style.opacity = op + 0.1;
+        console.log(cell.style.opacity);
+      }
     })
   );
 }
 
 // LOGIC FLOW
+
+createGrid(25);

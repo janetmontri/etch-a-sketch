6e343038a6f4356a31e5af44fbbc65bf56f4bcef
Janet Lee <jlee25@gmail.com>
2020-10-20 11:15:51 -0400

    stopping point for now; screenshot feature not added yet

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Project for The Odin Project under Javascript Basics
+
 https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-sketch-project
+
+10/20/2020: Learned a lot on this project, but there are also many things that I wanted to do but decided that I would have to figure it out later, for the sake of not losing momentum with the curriculum. Hoping to come back to this project to make the user experience smoother with the color picker changes, make the crayonEffect consistent (at this point, if you switch back and forth between colorWells and the rainbow magic marker effect, you lose the shading effect; all coloring becomes opaque), and add a Screenshot feature.
--- script.js
+++ script.js
@@ -2,9 +2,19 @@
 
 function promptForNumber() {
   var numOfRows = prompt(
-    "How many rows would you like? Enter a number between 1 and 75."
+    "How many rows would you like? Enter a number between 4 and 75."
   );
-  createGrid(numOfRows);
+
+  if (numOfRows === null) {
+    return;
+  } else {
+    while (numOfRows < 4 || numOfRows > 75) {
+      var numOfRows = prompt(
+        "Oops, that's an invalid response.  Enter a number between 4 and 75."
+      );
+    }
+    createGrid(numOfRows);
+  }
 }
 
 function createGrid(numOfRows) {
@@ -25,34 +35,140 @@ function createGrid(numOfRows) {
     squares.appendChild(cell);
   }
 
-  darkenCells("blue");
+  var pickedColor1 = document.getElementById("colorWell-1");
+  console.log(pickedColor1.value);
+  crayonEffect(pickedColor1.value);
 }
 
-function colorCells(cellColor) {
-  let cells = document.querySelectorAll(".cell");
+function crayonEffectOnLoad(cellColor) {
+  var cells = document.querySelectorAll(".cell");
   cells.forEach((cell) =>
     cell.addEventListener("mouseenter", () => {
-      cell.style.setProperty("--cell-background-color", `${cellColor}`);
+      let op = parseFloat(cell.style.opacity);
+      if (op < 1) {
+        cell.style.opacity = op + 0.1;
+      }
+      cell.style.backgroundColor = cellColor;
     })
   );
 }
 
-function darkenCells(cellColor) {
+function startupBgColor() {
+  bgcolor = document.querySelector("#bgcolor");
+  bgcolor.value = bgColorDefault;
+  bgcolor.addEventListener("input", updateFirstBgColor, false);
+  bgcolor.addEventListener("change", changeBgColor, false);
+}
+
+function updateFirstBgColor(cellColor) {
+  var cells = document.querySelectorAll(".cell");
+  cells.forEach(
+    (cell) => (cell.style.backgroundColor = cellColor.target.value)
+  );
+}
+
+function changeBgColor(cellColor) {
+  var cells = document.querySelectorAll(".cell");
+  cells.forEach(
+    (cell) =>
+      //   cell.style.setProperty("--cell-background-color", `${cellColor}`);
+      (cell.style.backgroundColor = cellColor.target.value)
+  );
+}
+
+function startupFirstColor() {
+  crayonColor = document.querySelector("#colorWell-1");
+  crayonColor.value = firstColorDefault;
+  crayonColor.addEventListener("input", updateFirstColor, false);
+  crayonColor.addEventListener("change", crayonEffect, false);
+}
+
+function updateFirstColor(cellColor) {
   var cells = document.querySelectorAll(".cell");
   cells.forEach((cell) =>
     cell.addEventListener("mouseenter", () => {
-      //   cell.style.setProperty("--cell-background-color", `${cellColor}`);
-      //   cell.style.setProperty("--cell-opacity", "0.1");
-      cell.style.backgroundColor = `${cellColor}`;
       let op = parseFloat(cell.style.opacity);
       if (op < 1) {
         cell.style.opacity = op + 0.1;
-        console.log(cell.style.opacity);
       }
+      cell.style.backgroundColor = cellColor.target.value;
+    })
+  );
+}
+
+function startupSecondColor() {
+  crayonColor = document.querySelector("#colorWell-2");
+  crayonColor.value = secondColorDefault;
+  crayonColor.addEventListener("input", updateSecondColor, false);
+  crayonColor.addEventListener("change", crayonEffect, false);
+}
+
+function updateSecondColor(cellColor) {
+  var cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) =>
+    cell.addEventListener("mouseenter", () => {
+      let op = parseFloat(cell.style.opacity);
+      if (op < 1) {
+        cell.style.opacity = op + 0.1;
+      }
+      cell.style.backgroundColor = cellColor.target.value;
+    })
+  );
+}
+
+function crayonEffect(cellColor) {
+  var cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) =>
+    cell.addEventListener("mouseenter", () => {
+      let op = parseFloat(cell.style.opacity);
+      if (op < 1) {
+        cell.style.opacity = op + 0.1;
+      }
+      cell.style.backgroundColor = cellColor;
+    })
+  );
+}
+
+function startupRainbowColor() {
+  rainbowColor = document.querySelector("#rainbowColor");
+  rainbowColor.addEventListener("click", changeRainbowColor);
+}
+
+function changeRainbowColor() {
+  var cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) =>
+    cell.addEventListener("mouseenter", () => {
+      let op = parseFloat(cell.style.opacity);
+      if (op < 1) {
+        cell.style.opacity = op + 0.1;
+      }
+      let colors = [
+        "rgb(255, 0, 0, 1)",
+        "rgb(255, 165, 0, 1)",
+        "rgb(255, 255, 0, 1)",
+        "rgb(0, 128, 0, 1)",
+        "rgb(0, 0, 255, 1)",
+        "rgb(75, 0, 130, 1)",
+        "rgb(12, 10, 127, 1)",
+      ];
+      let randomColor = colors[Math.floor(Math.random() * colors.length)];
+      cell.style.backgroundColor = randomColor;
     })
   );
 }
 
 // LOGIC FLOW
 
-createGrid(25);
+var bgColorDefault = "#FFFFFF";
+var firstColorDefault = "#1E90FF";
+var secondColorDefault = "#B22222";
+
+window.addEventListener("load", startupBgColor, false);
+window.addEventListener("load", startupFirstColor, false);
+window.addEventListener("load", startupSecondColor, false);
+window.addEventListener("load", startupRainbowColor, false);
+
+createGrid(35);
+crayonEffectOnLoad(firstColorDefault);
+
+// https://ngvuong.github.io/Etch-A-Sketch/ for using ctrl and shift buttons to draw/erase
